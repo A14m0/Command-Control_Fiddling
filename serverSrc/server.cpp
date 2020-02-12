c7bd338bea0f681bb183e4f5dff61b2dca79ef9a
Worldotank1287 <nicolas.p.ammann@gmail.com>
2020-02-12 16:13:12 -0700

    updated paths

--- serverSrc/server.cpp
+++ serverSrc/server.cpp
@@ -5,6 +5,7 @@
 #endif
 
 
+// TODO: RETHINK THIS WHOLE THING
 #ifdef HAVE_ARGP_H
 const char *argp_program_version = "libssh server example "
     SSH_STRINGIFY(LIBSSH_VERSION);
@@ -208,11 +209,6 @@ ConnectionInstance::ConnectionInstance(){
         mkdir("out", 0755);
         printf("Server: initialized directory 'out'\n");
     }
-// TODO: NOT SURE WE NEED THIS ANYMORE?
-    if (stat("modules", &st) == -1) {
-        mkdir("modules", 0755);
-        printf("Server: initialized directory 'modules'\n");
-    }
 
     if (stat(DATA_FILE, &st) == -1) {
         int fd2 = open(DATA_FILE, O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH);
@@ -410,95 +406,6 @@ void ConnectionInstance::get_ports(char *ptr){
 /*Place holder*/
 void ConnectionInstance::reverse_shell(){}
 /*Handles reverse shell connections and forwarding*/
-/*
-void ConnectionInstance::reverse_shell(class ServerTransport *transport){
-    int port = (rand() % (65535-2048))+ 2048;
-    pClientNode node = transport->get_node();
-    char agent_buffer[BUFSIZ];
-    char manager_buffer[BUFSIZ];
-    char logbuff[BUFSIZ];
-    int server_fd, manager, rc; 
-    struct sockaddr_in address; 
-    int opt = 1; 
-    int addrlen = sizeof(address); 
-
-    memset(agent_buffer, 0, sizeof(agent_buffer));
-    memset(manager_buffer, 0, sizeof(manager_buffer));
-    memset(logbuff, 0, sizeof(logbuff));
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        return; 
-    } 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) 
-    { 
-        perror("setsockopt"); 
-        return; 
-    } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons(port); 
-       
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        return; 
-    }
-
-
-
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        return; 
-    } 
-    if ((manager = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
-    while (manager && ssh_channel_is_open(node->data->chan))
-    {
-        // Get stuff from the agent
-        while(rc != 0){
-            rc = ssh_channel_read(node->data->chan, agent_buffer, sizeof(agent_buffer), 0);
-            if (rc == SSH_ERROR)
-            {
-                sprintf(logbuff, "Client %s: Failed to forward data: %s\n", node->data->id, ssh_get_error(node->data->session));
-                this->logger->log(logbuff);
-                return;
-            }
-            // send info to manager socket
-            write(manager, agent_buffer, rc);
-            memset(agent_buffer, 0, sizeof(agent_buffer));
-        }
-        // Get stuff from the manager socket
-        while(rc != 0){
-            rc = read(manager, manager_buffer, sizeof(manager_buffer));
-            if (rc == SSH_ERROR)
-            {
-                sprintf(logbuff, "Manager %s: Failed to forward data: %s\n", node->data->id, ssh_get_error(node->data->session));
-                this->logger->log(logbuff);
-                return;
-            }
-            // send info to manager socket
-            ssh_channel_write(node->data->chan, manager_buffer, sizeof(manager_buffer));
-            memset(manager_buffer, 0, sizeof(manager_buffer));
-        }
-
-        
-
-        
-    }
-    printf("Someone disconnected...\n");
-    
-    return; 
-
-}*/
 
 /*Handler for agent connections and flow*/
 void ConnectionInstance::agent_handler(){    
@@ -600,10 +507,6 @@ void ConnectionInstance::agent_handler(){
 
 /*Initialized the connection and prepares data structures for handlers*/
 void *ConnectionInstance::handle_connection(void *input){
-    printf("Thread created\n");
-
-    // TODO: MOVE THIS INTO TRANSPORT CLASS
-
     class ConnectionInstance *instance = (class ConnectionInstance *)input;
 
     // creates classes and instances
@@ -650,8 +553,7 @@ void print_clientDat(pClientDat str){
 
 /*Funny enough, this is the main function*/
 int main(int argc, char **argv){
-// TODO: UPDATE SIGHANDLERS TO MODERN STUFF
-// TODO: ALSO UPDATE THIS TO FORCE SETUP TO GO THROUGH A TRANSPORT
+    // TODO: UPDATE SIGHANDLERS TO MODERN STUFF
 
     // set up signal handlers
     //struct sigaction sigIntHandler;
