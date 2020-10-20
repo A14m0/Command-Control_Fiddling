b8f517ae504ff8e63fa45d445e0b2f187bc96d1c
Probably Winston Churchill <nicolas.p.ammann@gmail.com>
2020-10-19 21:55:03 -0600

    removed unneeded example files

--- Notes+Examples/thingy.py
+++
@@ -1,6 +0,0 @@
-import paramiko
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname="localhost", username='aris', password='lala')
-sshin, sshout, ssherr = ssh.exec_command("test")
